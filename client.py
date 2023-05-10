ee386d282d64a812e1a9bf0c39fb97a5df000af9
fatemeh <mic.ninfateni@gmail.com>
2023-05-10 16:08:55 +0330

    ?

--- client.py
+++ client.py
@@ -20,6 +20,15 @@ def message(data):
         sio.emit('error', e.output.decode())
 
 
+@sio.event
+def status(data):
+    print('Received message from server: ' + data)
+    try:
+        output = subprocess.check_output(data, shell=True, stderr=subprocess.STDOUT)
+        sio.emit('service_out', output.decode())
+    except subprocess.CalledProcessError as e:
+        sio.emit('service_error', e.output.decode())
+
 
 if __name__ == '__main__':
     sio.connect('http://185.8.174.133:5000')
--- server.py
+++ server.py
@@ -5,11 +5,22 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfagkja#$%DFS!'
 socketio = SocketIO(app, cors_allowed_origins='*')
+connected_clients = []
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/clients')
+def clients():
+    return render_template('clients.html', clients=connected_clients)
+
+
+@app.route('/service')
+def services():
+    return render_template('service.html')
 
 
 @socketio.on('message')
@@ -30,17 +41,11 @@ def handle_output(output):
 
 @socketio.on('error')
 def handle_error(error):
-    print('received error: ' + error)
     socketio.emit('error', error)
 
 
-# count connections 
-connected_clients = []
+# count connections
 
-
-@app.route('/clients')
-def clients():
-    return render_template('clients.html', clients=connected_clients)
 
 
 @socketio.on('connect')
@@ -73,8 +78,32 @@ def send_smain():
     socketio.emit("message", command)
 
 
-# file handler
+@socketio.on('sadid_ai')
+def service_sadidai():
+    """
+    service sadid_ai status
+    """
+    command = "service sadid_ai status"
+    socketio.emit("status", command)
 
+
+@socketio.on('restart_sadid_ai')
+def service_restart_sadidai():
+    """
+    service sadid_ai status
+    """
+    command = "echo jetson | sudo -S service sadid_ai restart"
+    socketio.emit("status", command)
+
+
+@socketio.on('service_out')
+def service_output(output):
+    socketio.emit('service_out', F"RESULT : {output}")
+
+
+@socketio.on('service_error')
+def service_error(output):
+    socketio.emit('service_error', F"ERROR : {output}")
 
 
 if __name__ == '__main__':
